e52aee92965f1270b699fe306c8df6db24f780d8
Lucas Dainez <lucdainez@gmail.com>
2022-07-11 15:45:33 -0300

    feat(fetchProducts): implementar requisição de uma API
    Aqui foi implementada uma função que faz uma requisição de uma API

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (args) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${args}`; 
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
