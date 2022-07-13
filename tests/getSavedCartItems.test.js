2499753f797cb0f7258490f642788fd9a539415b
Lucas Dainez <lucdainez@gmail.com>
2022-07-13 14:01:55 -0300

    feat(getSavedCartItems): Função criada, implementada e testada
    Função criada para requerir o localStorage, além de também ter sua cobertura de testes implementadas

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,13 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('ao executar "getSavedCartItems", o método "localStorage.getItem" é chamado', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+  }); 
+
+  it('ao executar "getSavedCartItems", o método "localStorage.getItem" é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
